d042be3277c0d3abb59dea7af71ee92ff0964fee
Rahul Siyanwal <rsiyanwal@gmail.com>
2023-04-28 18:08:01 +0530

    Update Motion-Detection-Basic.ino

--- Motion-Detection-Basic.ino
+++ Motion-Detection-Basic.ino
@@ -1,3 +1,15 @@
+/*
+The given code is an example of how to use a PIR sensor with an Arduino board. 
+It starts by defining the pin number of the PIR sensor and the threshold value for detecting motion. 
+In the setup function, the serial communication is initialized, and the pins are defined as inputs and outputs. 
+Then, in the loop function, the duration of the high signal is measured using the pulseIn function. 
+If the duration is greater than the threshold value, it indicates that motion has been detected, and the built-in 
+LED is turned on for 25 milliseconds and then turned off for another 25 milliseconds. 
+The serial monitor also displays a message indicating that motion has been detected. If the duration is less than the threshold value, 
+it indicates that no motion has been detected, and the LED remains off while the serial monitor displays a "No Motion" message. 
+The delays in the code are used to debounce the signal and avoid false positives.
+*/
+
 #define PIR 0
 #define PIR_THRESH 10000
  
