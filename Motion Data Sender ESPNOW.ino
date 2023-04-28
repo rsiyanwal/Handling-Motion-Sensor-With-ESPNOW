7ee6c7518f85f86161e0a99e672b8e93286dab7e
Rahul Siyanwal <rsiyanwal@gmail.com>
2023-04-28 18:22:05 +0530

    Update Motion Data Sender ESPNOW.ino

--- Motion Data Sender ESPNOW.ino
+++ Motion Data Sender ESPNOW.ino
@@ -1,49 +1,69 @@
-/**********************************************************
-ESP-NOW; Sending Motion Sensor data from Web to the Spider (Sender)
+/*
+This is a code for ESP8266-based microcontrollers that sends motion sensor data from the web to the Spider using ESP-NOW protocol. 
+The program includes the necessary libraries for ESP-NOW and ESP8266WiFi.
 
-use sudo iwlist wlan0 scanning to check the channels of wifi
-sudo iwlist wlan0 channel
-iwlist --help
+The code sets up the ESP8266 module as a Wi-Fi station and initializes the ESP-NOW protocol. 
+It registers a send callback function and adds a peer with a specific MAC address.
 
-change wifi channel: 
-sudo ifconfig wlan0 down
-sudo iwconfig wlan0 channel 1
-sudo ifconfig wlan0 up
-***********************************************************/
+The code defines a data structure named Message, which consists of two elements: a character array of size 32 to store the current date and time, 
+and another character array of size 32 to store the motion sensor value.
 
-/* Including the Libraries */
+In the loop, the code checks the analog output of a PIR motion sensor connected to pin A0. 
+If the analog output is above a certain threshold, the program sets the LED pin D0 to HIGH, 
+populates the message structure with the current date and time and a string "E7", which represents the motion sensor data, 
+and sends the message via ESP-NOW to the registered peer with a specific MAC address. 
+If the analog output is below the threshold, the program sets the LED pin D0 to LOW.
+
+The program also includes a callback function named OnDataSent, which prints the status of the last packet sent by the ESP-NOW protocol.
+*/
+
+// Including the Libraries
 #include<espnow.h>
 #include<ESP8266WiFi.h>
 #include<time.h>
 
-/* Variables */
+/* 
+Variables 
+*/
 unsigned long lastTime = 0;
 unsigned long timerDelay = 8000; //eight seconds
 const int PIRSensorOutPin = A0;
 time_t t;
 
-/* MAC Addresses */
-//uint8_t broadcastAddress[] = {0x30, 0x83, 0x98, 0x80, 0x3A, 0xBF};
-//uint8_t broadcastAddress[] = {0x98, 0xCD, 0xAC, 0x30, 0xF9, 0x06};
-uint8_t broadcastAddress[] = {0xE8, 0xDB, 0x84, 0xE0, 0x4A, 0x6C};
+/*
+BROADCAST ADDRESS is the MAC address of the NodeMCU receiver and can be obtained using the algorithm described in https://github.com/rsiyanwal/NodeMCU-MAC-Address. 
+MESSAGE is a variable that holds the message to be sent when motion is detected. We have used structures in the C++ programming language for this purpose. 
+The variable SIZE specifies the size of the message to be sent. It is important to note that for ESP-NOW, the message size should not exceed 250 bytes.
+*/
+// Receiver's MAC Address
+uint8_t broadcastAddress[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
 
-/* Defining the Data Structure */
+// Defining the Data Structure
 typedef struct struct_message{
   char a[32];
-  //int b;
   char b[32];
 }Message;
 
-/* Create Message */
+// Create Message
 Message message;
 
-/* CallBack function called when data is sent */
+/* 
+CallBack function called when data is sent 
+*/
+// Checking if the message is successfully delivered or not.
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus){
  Serial.print("\r\nLast Packet Sent Status:\t");
  Serial.println(sendStatus == 0 ? "Delivery Success" : "Delivery Fail"); 
 }
 
-/* Run Once */
+/*
+The process involves the following steps:
+- Set up Serial monitor
+- Set WiFi mode
+- Initialize ESP-NOW protocol
+- Register callback function
+- Register peer using its MAC address.
+*/
 void setup(){
   // Serial Monitor
   Serial.begin(115200);
@@ -70,7 +90,9 @@ void setup(){
 
 }
 
-/* Runs in a loop */
+/*
+Sending Motion Sensor Data
+*/
 void loop(){
   time(&t);
   if((millis() - lastTime) > timerDelay){
